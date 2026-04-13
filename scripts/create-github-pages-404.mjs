fe32619058b9e634ebc4edbb205064a9146bbf4d
21fpuna <ale21par@fpuna.edu.py>
2026-04-12 22:28:41 -0300

    Delete scripts directory

--- scripts/create-github-pages-404.mjs
+++
@@ -1,6 +0,0 @@
-import { copyFile } from 'node:fs/promises';
-import { resolve } from 'node:path';
-
-const docsDir = resolve('docs');
-
-await copyFile(resolve(docsDir, 'index.html'), resolve(docsDir, '404.html'));
